b7dbe80b960fb31451f41a90794c6c3ebcb58e8a
andremlourdes <andremarcelodelourdes@gmail.com>
2025-11-16 17:41:20 -0300

    Second commit

---
+++ src/main/java/com/challenge/transaction_api/business/services/Statisticsservice.java
@@ -0,0 +1,4 @@
+package com.challenge.transaction_api.business.services;
+
+public class Statisticsservice {
+}
---
+++ src/main/java/com/challenge/transaction_api/controller/dtos/StatisticsReponseDTO.java
@@ -0,0 +1,4 @@
+package com.challenge.transaction_api.controller.dtos;
+
+public record StatisticsReponseDTO() {
+}
